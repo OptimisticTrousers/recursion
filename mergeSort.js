d3b470948cd76f535e4fa587767b5649060dbd04
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-11 09:00:50 -0400

    Solving merge sort and renaming fibs and fibsRec

---
+++ mergeSort.js
@@ -0,0 +1,46 @@
+function mergeSort(array) {
+  if (array.length < 2) {
+    return array;
+  }
+
+  const firstHalf = array.slice(0, array.length / 2);
+  const secondHalf = array.slice(array.length / 2);
+  mergeSort(firstHalf);
+  mergeSort(secondHalf);
+  merge(array, firstHalf, secondHalf);
+}
+
+function merge(array, firstHalf, secondHalf) {
+  let firstIndex = 0;
+  let secondIndex = 0;
+  let arrayIndex = 0;
+
+  while (firstIndex < firstHalf.length && secondIndex < secondHalf.length) {
+    if (firstHalf[firstIndex] < secondHalf[secondIndex]) {
+      array[arrayIndex] = firstHalf[firstIndex];
+      firstIndex++;
+    } else {
+      array[arrayIndex] = secondHalf[secondIndex];
+      secondIndex++;
+    }
+    arrayIndex++;
+  }
+
+  while (firstIndex < firstHalf.length) {
+    array[arrayIndex] = firstHalf[firstIndex];
+    firstIndex++;
+    arrayIndex++;
+  }
+
+  while (secondIndex < secondHalf.length) {
+    array[arrayIndex] = secondHalf[secondIndex];
+    secondIndex++;
+    arrayIndex++;
+  }
+}
+
+const array = [9, 10, 16, 1, 5];
+
+mergeSort(array);
+
+console.log(array);
