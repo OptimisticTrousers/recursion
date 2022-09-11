ceb1e45e1e09aedcc15926e403959f611e6ee1f8
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-11 10:15:57 -0400

    Starting problem 1 from Project Euler

---
+++ euler1.js
@@ -0,0 +1,10 @@
+function sumOfMultiplesOf3And5(number) {
+  if (number === 3) {
+    return number;
+  } else if (number === 5) {
+    return number;
+  }
+
+}
+
+console.log(sumOfMultiplesOf3And5(9));
